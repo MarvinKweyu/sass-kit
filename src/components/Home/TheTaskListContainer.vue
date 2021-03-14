2f68ee0683dc7d284cd154e07a9ef525a6d63d10
MarvinKweyu <mkweyu1@gmail.com>
2021-03-14 11:03:09 +0300

    initialize tasks board on dashboard home
    chore: create tasks timeline board
    
    chore: calc progress of tasks
    
    style: adjsut task header section
    
    chore: use Poppins font
    
    -show edit options on same line as task status button
    
    chore: add images alt and sizes
    
    style: UI adjustements to mid section of dashboard home card list
    
    style: add style binding to tasks status btn
    
    style: add margin and padding to tasks status on home page
    
    chore: initalize tasks graph
    
    chore: change to required colors of active , ended and completed tasks
    
    chore: display highest value in donut for tasks
    
    chore: display donut with percentage width
    
    style: position donut at the center of task page
    
    style: transform btn text to initial case

---
+++ src/components/Home/TheTaskListContainer.vue
@@ -0,0 +1,173 @@
+<template>
+  <q-card class="my-card">
+    <q-card-section>
+      <TheTasksTimeBoard
+        :taskAnalysis="taskAnalysis"
+        class="q-mb-md time-board"
+      />
+      <div v-for="task in allTasks.splice(0, 3)" :key="task.name">
+        <TaskDetailCard :taskDetail="task" class="q-mb-sm task-card" />
+      </div>
+      <div class="flex justify-center show-more">Show more</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import TaskDetailCard from "@/components/Home/TaskDetailCard";
+import TheTasksTimeBoard from "@/components/Home/TheTasksTimeBoard";
+export default {
+  name: "TheTaskListContainer",
+  components: {
+    TaskDetailCard,
+    TheTasksTimeBoard
+  },
+  data() {
+    return {
+      allTasks: [
+        {
+          title: "Send benefit review by Sunday",
+          type: "Reminder",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "George Fields"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Ended"
+        },
+        {
+          title: "Office meet-up",
+          type: "Event",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Lindsey Stroud"
+          },
+          taskState: "Completed"
+        },
+        //  for purpose of the analytics page,the second object has
+        // been duplicated
+        {
+          title: "Invite to office meet-up2",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up3",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up4",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up7",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up8",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up9",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "Completed"
+        },
+        {
+          title: "Invite to office meet-up10",
+          type: "Call",
+          dueDate: "December 23, 2018",
+          adressee: {
+            image: "https://cdn.quasar.dev/img/avatar.png",
+            name: "Rebecca Moore"
+          },
+          taskState: "ended"
+        }
+      ]
+    };
+  },
+  computed: {
+    //  this will parse the overal analytics for task count
+    // display correct completed vs total tasks count based on fetch
+    taskAnalysis() {
+      const completed = this.allTasks.filter(
+        task => task.taskState === "Completed"
+      );
+      return {
+        taskTotal: this.allTasks.length,
+        tasksCompleted: completed.length
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.my-card {
+  height: 99%;
+}
+.show-more {
+  /* width: 160px; */
+  height: 20px;
+  /* margin: 15px 87px 0 89px; */
+  font-size: 13px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: 0.13px;
+  text-align: center;
+  color: #109cf1;
+}
+.task-card {
+  cursor: pointer;
+}
+.time-board {
+  margin: 24px 0;
+  border-bottom: 1px solid #ebeff2;
+}
+</style>
