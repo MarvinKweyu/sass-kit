7435365cffbaa9d1470ae40b5f21019b9dacc680
MarvinKweyu <mkweyu1@gmail.com>
2021-03-23 15:14:52 +0300

    chore: add circleci config

--- tests/unit/tasksCard.spec.js
+++ tests/unit/tasksCard.spec.js
@@ -1,9 +1,17 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Tasks from "../../src/components/Home/Tasks";
 
 describe("Tasks.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(Tasks, {});
+  });
+
+  test("component is rendered", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
   test("percentage to contain 60%", () => {
-    const wrapper = shallowMount(Tasks, {});
     expect(wrapper.text()).toContain("60");
   });
 });
