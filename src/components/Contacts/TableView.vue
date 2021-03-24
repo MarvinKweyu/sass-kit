d29d53384d449a008bf46adcbc69e507a135e86a
MarvinKweyu <mkweyu1@gmail.com>
2021-03-24 09:54:14 +0300

    style: contact table box shadow changes

--- src/components/Contacts/TableView.vue
+++ src/components/Contacts/TableView.vue
@@ -334,7 +334,7 @@ td {
   margin: 1px 0 0;
   padding: 20px 24px;
 }
-.table-row :hover {
-  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
-}
+/*tr :hover {*/
+/*  box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);*/
+/*}*/
 </style>
